d9db9834dba47f837a1eb68dafca6db1a806263e
zealuu <zealuu@qq.com>
2024-08-26 15:01:04 +0800

    zealuu 首次部署

---
+++ src/main/java/com/evaluate/ai_evaluate/AiEvaluateApplication.java
@@ -0,0 +1,13 @@
+package com.evaluate.ai_evaluate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiEvaluateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiEvaluateApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/evaluate/ai_evaluate/AiEvaluateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.evaluate.ai_evaluate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiEvaluateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
